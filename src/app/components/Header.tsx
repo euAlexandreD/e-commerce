db9418f5ee2d6617011504b8aee24b5146b032aa
Alexandre Portella <eualexandre.dev@gmail.com>
2024-04-19 00:28:37 -0300

    adicionado menu lateral

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,13 +1,22 @@
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import { MenuSquareIcon, ShoppingCartIcon } from "lucide-react";
+import {Sheet ,SheetContent, SheetHeader, SheetTrigger } from "@/components/ui/sheet";
+import { MenuIcon, ShoppingCartIcon } from "lucide-react";
 
 const Header = () => {
     return (
         <Card className="flex items-center justify-between p-[1.875]">
-            <Button size="icon" variant="outline">
-                <MenuSquareIcon/>
-            </Button>
+            <Sheet>
+                <SheetTrigger asChild>
+                    <Button size="icon" variant="outline">
+                        <MenuIcon/>
+                    </Button>
+                </SheetTrigger>
+
+                <SheetContent side="left">
+                    <SheetHeader className="text-left text-lg font-semibold">Menu</SheetHeader>
+                </SheetContent>
+            </Sheet>
 
             <h1 className="font-semibold text-lg"><span className="text-primary">Circuit</span> Corner</h1>
 
@@ -15,7 +24,7 @@ const Header = () => {
                 <ShoppingCartIcon/>
             </Button>
         </Card>
-    )
+    );
 }
 
 export default Header;
